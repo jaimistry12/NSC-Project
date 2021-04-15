186aabcff31c0b260b42114e6be0984e66ba202b
jaimistry12 <mistryjai050@gmail.com>
2021-04-14 23:55:02 -0500

    firebase setup

---
+++ my-app/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyAswmgZA9aiKzZSuRJp9KSZ-S8n4V3VsyI",
+    authDomain: "nsc-project-4888b.firebaseapp.com",
+    databaseURL: "https://nsc-project-4888b-default-rtdb.firebaseio.com",
+  }
+  
+  export default config;
